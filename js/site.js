cd56824886fc947ea873432a071368da3fff309c
jose <167133923+joseyty@users.noreply.github.com>
2025-12-15 01:11:54 -0300

    atualização

--- js/site.js
+++ js/site.js
@@ -2,12 +2,12 @@ function comprarItem(){
  let estoque =10;
 
 
-     if( estoque >0){
+     if( estoque >=10){
     let cafe = "cafe comprado com sucesso";
     document.getElementById("mensagem").textContent = cafe;
      mensagem.style.color = "green";
 
-     } else if(estoque<=0){
+     } else if(estoque ===0){
         let erro ="Fora de estoque";
         document.getElementById("mensagem").textContent = erro;
         mensagem.style.color = "red";
